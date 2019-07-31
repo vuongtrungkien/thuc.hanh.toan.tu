52d7ef8a2199a51e66724cf08cc681d14e1b0930
vuongtrungkien <nobitaa6@gmail.com>
2019-07-31 17:42:48 +0800

    bai thuc hanh

---
+++ congthuctinh.js
@@ -0,0 +1,11 @@
+let inputwidth;
+let inputheight;
+
+inputwidth = prompt("nhap vao chieu rong")
+inputheight = prompt("nhap vao chieu cao")
+
+let width = parseInt(inputwidth);
+let height = parseInt(inputheight);
+
+let area = width * height;
+document.write("dien tich cua hinh chu nhat la:" + area);
